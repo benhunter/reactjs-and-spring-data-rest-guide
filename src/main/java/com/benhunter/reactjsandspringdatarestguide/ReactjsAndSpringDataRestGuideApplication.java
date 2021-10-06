8f996e92e3b1c5771516eb5ff90001c97be4db2a
Ben Hunter <code@benhunter.me>
2021-10-06 01:48:43 -0500

    Spring Initializr

---
+++ src/main/java/com/benhunter/reactjsandspringdatarestguide/ReactjsAndSpringDataRestGuideApplication.java
@@ -0,0 +1,13 @@
+package com.benhunter.reactjsandspringdatarestguide;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReactjsAndSpringDataRestGuideApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReactjsAndSpringDataRestGuideApplication.class, args);
+	}
+
+}
